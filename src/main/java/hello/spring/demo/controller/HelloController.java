b89db09cb8a128171174271b7119f92c9693a803
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2021-09-30 06:33:56 +0900

    practice3

--- src/main/java/hello/spring/demo/controller/HelloController.java
+++ src/main/java/hello/spring/demo/controller/HelloController.java
@@ -3,7 +3,9 @@ package hello.spring.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,4 +20,32 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello hellAPi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
