25510d5d1a0579be5a674dfde0573ae0ac2b9ca4
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2021-10-16 03:16:02 +0900

    practice7

--- src/main/java/hello/spring/demo/controller/MemberController.java
+++ src/main/java/hello/spring/demo/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.spring.demo.controller;
 
+import hello.spring.demo.domain.Member;
 import hello.spring.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,4 +16,19 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping(value = "/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
+
